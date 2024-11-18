670dc895669f545ae5eb0377a427a4afca3cf746
Ekta Chaudhry <ekta.chaudhry0701@gmail.com>
2024-11-18 19:53:11 +0530

    Learnt atoms in recoil

--- Basics/recoil/src/App.jsx
+++ Basics/recoil/src/App.jsx
@@ -1,7 +1,45 @@
 import { useState } from 'react'
+import {RecoilRoot, useRecoilValue, useSetRecoilState} from 'recoil'
+import { counterAtom } from './store/atoms/counter'
 
 function App() {
-  
+  return <RecoilRoot>
+  <Counter></Counter>
+  </RecoilRoot>
+}
+
+function Counter() {
+    return <div>
+        <CurrentCounter></CurrentCounter>
+        <Increase ></Increase>
+        <Decrease ></Decrease>
+    </div>
+
+}
+
+function CurrentCounter() {
+    const count = useRecoilValue(counterAtom);
+    return <div>{count}</div>
+}
+
+function Increase() {
+    const setCount = useSetRecoilState(counterAtom);
+    function increase() {
+        setCount(c => c + 1);
+    }
+    return <div>
+        <button onClick={increase}>Increase</button>
+        </div>
+}
+
+function Decrease() {
+    const setCount = useSetRecoilState(counterAtom);
+    function decrease() {
+        setCount(c => c - 1);
+    }
+    return <div>
+        <button onClick={decrease}>Decrease</button>
+        </div>
 }
 
 export default App

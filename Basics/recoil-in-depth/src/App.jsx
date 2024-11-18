00fca4b81deba656d85ddc4f4f795c706fe84ec3
Ekta Chaudhry <ekta.chaudhry0701@gmail.com>
2024-11-19 00:54:21 +0530

    Added atoms and useRecoilValue

---
+++ Basics/recoil-in-depth/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { RecoilRoot, useRecoilValue } from 'recoil'
+import { jobsAtom, messagingAtom, networkAtom, notificationsAtom } from './atoms'
+
+function App() {
+  return <RecoilRoot>
+    <MainApp></MainApp>
+  </RecoilRoot>
+}
+
+function MainApp() {
+  const networkNotificationCount = useRecoilValue(networkAtom);
+  const jobsNotificationCount = useRecoilValue(jobsAtom);
+  const messagingNotificationCount = useRecoilValue(messagingAtom);
+  const notifsNotificationCount = useRecoilValue(notificationsAtom);
+  return <>
+    <button>Home</button>
+    <button>My Network({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button>
+    <button>Jobs({jobsNotificationCount >= 100 ? "99+" : jobsNotificationCount})</button>
+    <button>Messaging({messagingNotificationCount >= 100 ? "99+" : messagingNotificationCount})</button>
+    <button>Notifications({notifsNotificationCount >= 100 ? "99+" : notifsNotificationCount})</button>
+    <button>Me</button>
+  </>
+}
+
+export default App
